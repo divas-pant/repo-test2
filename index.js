a71e4bb5224c41c17159edd1496ae1bb03cbe173
divas-pant <divasthegame@gmail.com>
2023-03-06 14:47:47 +0100

    fix: new  branch movement

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ console.log("second fix");
 
 console.log("First Feature");
 
-console.log("Breaking One")
-console.log("Breaking Two")
+console.log("Breaking One");
+console.log("Breaking Two");
+
+console.log("Moved to Merge testing phase");
