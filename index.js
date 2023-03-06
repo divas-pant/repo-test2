590ab0cf995ed8a14055ad1d6728dc91f8015448
divas-pant <divasthegame@gmail.com>
2023-03-06 14:48:49 +0100

    feat: second feature

--- index.js
+++ index.js
@@ -9,3 +9,4 @@ console.log("Breaking Two");
 
 console.log("Moved to Merge testing phase");
 console.log("This marks first feature change for branch");
+console.log("This is the second feature of the branch");
