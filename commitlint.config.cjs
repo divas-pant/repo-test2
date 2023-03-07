e0c9863f83f92a04cfdb61e63ab12cc9d2ac0462
divas-pant <divasthegame@gmail.com>
2023-03-07 12:14:37 +0100

    fix: Commitlint config

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,8 +1,13 @@
 const RULES = {
     extends: ['@commitlint/config-conventional'],
     "rules": {
-        "body-full-stop": [1, "always", "."],
-        "body-case": [2, "always", "sentence-case"]
+        "body-full-stop": [1, "always", "."], // ensure body ends with a full stop, warn if not
+        "body-case": [2, "always", "sentence-case"], // ensure body is written in sentence case, error if not
+        "scope-case": [2, "always", "lower-case"], // ensures the scope is in lower case, error if not
+        "subject-case": [2, "always", "sentence-case"], // ensures subject is written in sentence case, error if not
+        "type-case": [2, "always", "lower-case"], // ensures type of the commit is in lower case, error if not
+        "subject-empty": [1, "never"], //makes subject of the message optional, wanrs itf not present
+        "type-empty": [1, "never"] // makes type of the message optional, warns if not present
 
     }
 }
