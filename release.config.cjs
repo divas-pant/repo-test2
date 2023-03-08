835cbbee6c17da9074744348f7e5071c7be34282
divas-pant <divasthegame@gmail.com>
2023-03-08 15:51:25 +0100

    chore: linting

--- release.config.cjs
+++ release.config.cjs
@@ -1,48 +1,47 @@
 module.exports = {
-  "ci": false,
-  "dryRun": false,
-  "release": {
-    "branches": ["master"],
-    "prepare": [
-      "@semantic-release/changelog",
-      "@semantic-release/npm",
-      {
-        "path": "@semantic-release/git",
-        "assets": [
-          "package.json", "CHANGELOG.md"
-        ],
-        "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-      }
-    ]
+  ci: false, // Setting this to false allows program to make release from local repository
+  dryRun: false, // Allows to make releases when running in local env, otherwise program prints out new version number
+  release: {
+      branches: ['master'], // Default branch from where the release was made
   },
-  "plugins": [
-    ["@semantic-release/commit-analyzer", {
-      "preset": "conventionalcommits",
-      "releaseRules": [
-        {"type": "docs", "scope":"README", "release": "patch"},
-        {"type": "refactor", "release": "patch"},
-        {"type": "style", "release": "patch"}
+  plugins: [
+      [
+          '@semantic-release/commit-analyzer', { // Analyzes the commit messages to identify type of release
+              preset: 'conventionalcommits', // Using Conventional commit as base for
+              releaseRules: [ // Custom release rules for types other than feat and fix
+                  {
+                      type: 'docs', // if type is docs and scope is readme file bump patch
+                      scope: 'README',
+                      release: 'patch',
+                  },
+                  {
+                      type: 'refactor', // if type is refactor, bump patch
+                      release: 'patch',
+                  },
+                  {
+                      type: 'style', // if type is style, bump patch
+                      release: 'patch',
+                  },
+              ],
+              parserOpts: { // Pass keywords for parser to look for Breaking change
+                  noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
+              },
+          },
       ],
-      "parserOpts": {
-        "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES"]
-      }
-    }],
-    ["@semantic-release/release-notes-generator", {
-      "preset": "conventionalcommits",
-      "parserOpts": {
-        "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
-      },
-      "writerOpts": {
-        "commitsSort": ["subject", "scope"]
-      }
-    }],
-    ["@semantic-release/exec", {
-      "publishCmd": "echo This is a test command "
-    }],
-    "@semantic-release/npm",
-    "@semantic-release/git",
-    ["@semantic-release/github", {
-      "assets": ["package.json"]
-    }]
-  ]
-}
+      [
+          '@semantic-release/release-notes-generator', { // Configure release notes generator
+              preset: 'conventionalcommits', // Use conventional commits as preset
+              writerOpts: {
+                  commitsSort: ['subject', 'scope'],
+              },
+          },
+      ],
+      '@semantic-release/npm', // Plugin to publish package on npm registry
+      '@semantic-release/git', // Plugin to publish package on git as a new tag
+      [
+          '@semantic-release/github', { // Plugin to publish package on github as new release
+              assets: ['package.json'], // Define any assets that needs to be published in github, for eg. the build folder
+          },
+      ],
+  ],
+};
