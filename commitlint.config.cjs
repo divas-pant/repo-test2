987fa3a4954b51c3a8388bc4caf9f4a2c92dad3a
divas-pant <divasthegame@gmail.com>
2023-03-08 10:24:27 +0100

    feat: case change to lower in subject and body

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -3,9 +3,9 @@ const RULES = {
     "rules": {
         "body-full-stop": [1, "always", "."], // ensure body ends with a full stop, warn if not
         "body-max-line-length": [0, "always"], // ensures body can be of any length
-        "body-case": [2, "always", "sentence-case"], // ensure body is written in sentence case, error if not
+        "body-case": [2, "always", "lower-case"], // ensure body is written in lower case, error if not
         "scope-case": [2, "always", "lower-case"], // ensures the scope is in lower case, error if not
-        "subject-case": [2, "always", "sentence-case"], // ensures subject is written in sentence case, error if not
+        "subject-case": [2, "always", "lower-case"], // ensures subject is written in lower case, error if not
         "type-case": [2, "always", "lower-case"], // ensures type of the commit is in lower case, error if not
         "subject-empty": [1, "never"], //makes subject of the message optional, wanrs itf not present
         "type-empty": [1, "never"] // makes type of the message optional, warns if not present
