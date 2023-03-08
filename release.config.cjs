cfe4dde69397736c2daa6408b7baad3d97193e3d
divas-pant <divasthegame@gmail.com>
2023-03-08 10:15:03 +0100

    feat(release): Prepare step chage

--- release.config.cjs
+++ release.config.cjs
@@ -2,7 +2,18 @@ module.exports = {
   "ci": false,
   "dryRun": false,
   "release": {
-    "branches": ["master"]
+    "branches": ["master"],
+    "prepare": [
+      "@semantic-release/changelog",
+      "@semantic-release/npm",
+      {
+        "path": "@semantic-release/git",
+        "assets": [
+          "package.json"
+        ],
+        "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      }
+    ]
   },
   "plugins": [
     ["@semantic-release/commit-analyzer", {
